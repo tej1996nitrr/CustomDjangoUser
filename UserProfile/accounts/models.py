1ae898f783a5f8c7988f44791c17ab1d95efa6fd
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-07-26 01:52:33 +0530

    custom user

--- UserProfile/accounts/models.py
+++ UserProfile/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
+from django.conf import settings
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserManager()
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(max_length=10)
+    bio = models.CharField(max_length=240, blank=True)
+    city = models.CharField(max_length=30, blank=True)
+    profile_pic = models.ImageField(null=True, blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.email
